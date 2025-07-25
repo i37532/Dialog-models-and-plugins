fd25efd711539fa183d1db0b1b8cb2b14ecb78c2
i37532 <1350181508@qq.com>
2025-07-25 16:11:24 +0800

    add get_pub

--- JustRun.py
+++ JustRun.py
@@ -2,14 +2,13 @@
 #!/usr/bin/env python3
 
 from robot.sdk.AbstractPlugin import AbstractPlugin
-from data_publisher import DataPublisher
+from data_publisher import get_publisher
 
 class Plugin(AbstractPlugin):
 
     def handle(self, text, parsed):
         self.say('不干了，不干啦，我要跑路啦，世界那么大，我想去看看', cache=True)
-        publisher = DataPublisher()
-        # 重复发送10次信息
+        publisher = get_publisher()
         while True:
             message = "status 跑路通知:我不干了"
             if publisher.send_message(message):
